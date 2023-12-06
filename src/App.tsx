61b4b2a5fd260c8cc2a50905a667fd760d7942b4
kez lee <kez.lee@outlook.com>
2023-12-06 16:23:42 +0800

    initialize react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import logo from './logo.svg';
+import background from './assets/images/bg-dark.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={background} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
